460342ea4b53a706211a638e8c005835f5fff679
Thomas <thomasbonnema@dhcp-v102-061.mobile.uci.edu>
2017-11-03 13:52:36 -0700

    first push

---
+++ assets/javascript/app.js
@@ -0,0 +1,42 @@
+//var timer
+//questions
+//answers
+
+//Set questions
+//Set list of questions
+//Set timer to countdown timer
+//Set comment
+
+var timer = 100;
+var questions = ["question1","question2","question3"];
+<ul class="answers"></ul>
+
+$("questions").html("question1");
+$("questions").html("questions2");
+$("questions").html("questions3");
+
+$("#timer").html(timer);
+
+$("#question1").html();
+
+    <input type="radio" id="A" value="-answer-"></input>
+    <input type="radio" id="B" value="-answer-"></input>
+    <input type="radio" id="C" value="-answer-"></input>
+    <input type="radio" id="D" value="-answer-"></input>
+
+<p "question1"></p>
+
+
+var listQuestion = function () {
+    if ( i < questions.length) {
+    document.getElementById
+    }
+}
+
+<p class ="question"> </p>
+<ul class ="answers">
+<input type="radio" name="q1" value="a" id="q1a"><label for="q1a">
+
+
+<div id = "results">
+<p><strong>Question 1</strong></p>
